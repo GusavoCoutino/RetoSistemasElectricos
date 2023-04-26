ca7f5e22db691f6d0be4d6ddecd111bcef0b6741
Gustavo Coutiño <76141180+GusavoCoutino@users.noreply.github.com>
2023-04-26 10:12:20 -0600

    Update entragable1SistemasElectricos.m

--- entragable1SistemasElectricos.m
+++ entragable1SistemasElectricos.m
@@ -1,4 +1,5 @@
-figure(1)
+clear;
+clc;
 
 xline(0,Color="red");
 xline(3, Color="red");
@@ -14,16 +15,60 @@ espacioPuntos = final-inicio;
 
 axis([0 18 0 18]);
 
-for i = 1:50
-    inicio(2) = inicio(2)+1;
-    final(2) = final(2)+1;
-    quiver(inicio(1), inicio(2),espacioPuntos(1),espacioPuntos(2),0);
-    hold on
-end
-
-x = input("Que coordenada en x quieres?");
+x = 3;
 y = input("Que coordenada en y quieres?");
-ultimoPuntoX = 12 - x+1;
-quiver(x,y, ultimoPuntoX, 0);
+ultimoPuntoX = 12 - x;
 
+x2 = 12;
+y2 = input("Que coordenada en y quieres para el segundo punto?");
+
+
+y3 = input("Que coordenada en y quieres? (Segunda coordenada)");
+y4 = input("Que coordenada en y quieres? (Tercera coordenada)");
+
+
+q = 11*10^-8;
+ke = 8.9875517873681764*10^9;
+
+
+dx = x2-x;
+dy = y2-y;
+r = sqrt(dx^2+dy^2);
+
+dex = ke*(q)/(r^3)*dx;        
+dey = ke*(q)/(r^3)*dy;
+
+dx2 = x2-x;
+dy2 = y3-y;
+r2 = sqrt(dx2^2+dy2^2);
+
+dex2 = ke*(q)/(r2^3)*dx2;        
+dey2 = ke*(q)/(r2^3)*dy2;
+
+dx3 = x2-x;
+dy3 = y4-y;
+r3 = sqrt(dx3^2+dy3^2);
+
+dex3 = ke*(q)/(r3^3)*dx3;        
+dey3 = ke*(q)/(r3^3)*dy3;
+
+% Suma final
+detXFinal = dex+dex2+dex3;
+detYFinal = dey+dey2+dey3;
+
+hold on
+plot(x2,y2)
+hold on
+plot(x2, y3)
+hold on
+plot(x2, y4);
+hold on
+quiver(x,y,detXFinal/5, detYFinal)
+hold on
+quiver(x2,y2,-detXFinal/5, detYFinal)
+hold on
+quiver(x2,y3,-detXFinal/5, detYFinal)
+hold on
+quiver(x2,y4,-detXFinal/5, detYFinal)
+hold on
 
